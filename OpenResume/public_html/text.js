b88a91d134bae4d13eb62316eb8a57da0b6033a3
matt <matt@mattflavell.net>
2016-02-02 10:42:30 -0500

    Added generic text to text.js

--- OpenResume/public_html/text.js
+++ OpenResume/public_html/text.js
@@ -3,91 +3,26 @@
  */
 
 /* headline for resume */
-var title="Matthew Flavell";
-var sub_title="Experienced IT professional with 18 years of industry experience";
+var title="Your Name";
+var sub_title="Your subtitle";
 
 /* resume areas */
-var menu_1="Cyber Security";
-var menu_2="Single Sign on";
-var menu_3="Application development";
-var menu_4="Consulting";
+var menu_1="Subject 1";
+var menu_2="Subject 2";
+var menu_3="Subject 3";
+var menu_4="Subject 4";
 
 /* text for areas */
-var text_1="<b>Cyber Security</b><br><br>\n\
-I have over 10 years of solid computer security experience with large organizations such at General Electric, Toyota, Pfizer. The broad scope of my work has included both hands on diagnosis of security events though to high level planning and the implementation of new security solutions.\n\
-<br><br>\n\
-\n\
-Some of my key achievements in the IT security field have been:<br>\n\
-<br><br>\n\
-<ul><li>The initial hands on response to several zero day attacks on the networks of major multi-national companies.\n\
-<br><br>\n\
-<li>Implementing security monitoring and assessment solutions at several major companies.\n\
-<br><br>\n\
-<li>Performing penetration tests and presenting the results in a understandable format.\n\
-<br><br>\n\
-<li>Using my software development knowledge to review new code and applications for potential vulnerabilities.\n\
-<br><br>\n\
-<li>Assessing corporate information security strategy and suggesting improvements to enhance network security.</ul>\n\
-";
-var text_2=" <b>Single Sign On</b><br><br>\n\
-                I have over 8 years experience with single sign on solutions such as SAML, Oauth, WS-Federation and CA SiteMinder. My work has involved implementing solutions and diagnosing technical issues for some of  CA's biggest and most valued customers.\n\
-\n\
-                My key achievements in the SSO field have been:\n\
-                <ul>    \n\
-                <li>Designing and implementing major multi-national SSO infrastructures for some of the worlds largest companies. \n\
-                    <br><br>\n\
-                <li>Performing tight integrations with both custom written and purchased customer applications.\n\
-                    <br><br>\n\
-                <li>Taking the lead in hands on diagnosis of complex technical issues that impact multiple applications across many regions.\n\
-                    <br><br>\n\
-                <li>Working closely with engineering teams to replicate, diagnose and resolve issues in the CA SSO solution.\n\
-                </ul>         ";
-var text_3="  <b>Application development</b><br><br>\n\
-                I have over 18 years of application development experience covering the Java, Visual Basic, C# and PHP. All of my recent projects have utilized Java with the hibernate and spring frameworks. I have extensive Java Script and Jquery experience, indeed this site was written from scratch using the Netbeans IDE. \n\
-\n\
-                My key application development achievements are:\n\
-                <ul>    \n\
-                <li>Wrote visual basic applications for several large automotive companies.\n\
-                    <br><br>\n\
-                <li>Lead the development of high security Java applications at GE.\n\
-                    <br><br>\n\
-                <li>Performed code reviews, evaluated bugs and wrote patches for a wide variety of Java applications.\n\
-                    <br><br>\n\
-                <li>Lead the development of user interface and chat software at GE.\n\
-                <br><br>\n\
-                <li>Updated and converted applications to take advantage of new emerging frameworks and technologies.\n\
-                </ul>";
-var text_4="<b>Consulting</b><br><br>\n\
-                I have over 15 years experience in the IT Consulting and pre-sales. The scope of my consulting experience is wide ranging and has encompassed networking, security, hardware and software. In the pre-sales environment I have responded to numerous proposals and lead many customer scoping meetings to ensure the systems I design exceed the customers expectations. I currently work as a security consultant for CA Technologies and have been assigned to some of CA's largest and most valuable customers.\n\
-\n\
-My key consulting achievements are:\n\
-<ul>\n\
-<li>Scoping, designing and implementing large scale solutions for multi-national customers.\n\
-<br><br>\n\
-<li>Performing detailed forensic analysis of customer environments with the focus on resolving errors, improving performance and enhancing stability.\n\
-<br><br>\n\
-<li>Leading the compilation of detailed analysis reports and presenting those reports to customers.\n\
-<br><br>\n\
-<li>Leading the resolution of customer issues by aligning subject matter experts across multiple domains.\n\
-<br><br>\n\
-<li>Becoming a seasoned subject matter expert in several technologies to become a leader in the CA mentoring program. \n\
-</ul>\n\
-                ";
+var text_1="Text 1";
+var text_2="Text 2";
+var text_3="Text 3";
+var text_4="Text 4";
 
 /*concact info*/
-var contact="<br><a href=\"mailto:matt@mattflavell.net?Subject=contact\" target=\"_top\">matt@mattflavell.net</a>\n\
-            <br><br><a href=\"https://www.linkedin.com/in/mflavell\" target=\"_blank\">https://www.linkedin.com/in/mflavell</a>\n\
-            <br><br><a href=\"https://github.com/mflavell\" target=\"_blank\">My Github</a>";
+var contact="Contact info";
 
 /*dash */
-var dash="  <h1>IT Security</h1>\n\
-            <h3>15 Years</h3><br><br>\n\
-            <h1>SSO</h1>\n\
-            <h3>8 Years</h3><br><br>\n\
-            <h1>App development</h1>\n\
-            <h3>18 Years</h3><br><br>\n\
-            <h1>Consulting</h1>\n\
-            <h3>15 Years</h3>          ";
+var dash="Dash";
 
 
 
