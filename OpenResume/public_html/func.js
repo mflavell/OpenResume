9af27dde8e6d7aec465ceb45c29d22f18807dcb2
matt <matt@mattflavell.net>
2016-02-02 10:37:38 -0500

    Inital version of openresume.

---
+++ OpenResume/public_html/func.js
@@ -0,0 +1,47 @@
+/* 
+ * This is the business end.
+ */
+
+function loadtext() {
+    $('#content').hide();
+    $('#content_text1').hide();
+    $('#content_text2').hide();
+    $('#content_text3').hide();
+    $('#content_text4').hide();
+    document.title = title;
+    document.getElementById("title").innerHTML=title;
+    document.getElementById("sub_title").innerHTML=sub_title;
+    document.getElementById("menu_1").innerHTML=menu_1;
+    document.getElementById("menu_2").innerHTML=menu_2;
+    document.getElementById("menu_3").innerHTML=menu_3;
+    document.getElementById("menu_4").innerHTML=menu_4;
+    document.getElementById("content_text1").innerHTML=text_1;
+    document.getElementById("content_text2").innerHTML=text_2;
+    document.getElementById("content_text3").innerHTML=text_3;
+    document.getElementById("content_text4").innerHTML=text_4;
+    document.getElementById("contact").innerHTML=contact;
+    document.getElementById("dash").innerHTML=dash;
+}
+
+var lastitem="";
+
+function movemenu(item) {
+    $(".title_menu").animate({left: '200px', top:'100px'});
+    $(".img-me").animate({left: '-950px', top:'210px'});
+    $(".contact").animate({left: '200px', top:'380px'});$(window).width();
+    var winwidth = $(window).width();
+    if (winwidth>1300) {
+        $(".dash").animate({left: '1150px', top:'100px'});
+    }
+    if (lastitem!="") {
+        $("#content_text"+lastitem).fadeOut(function() {
+             $("#content_text"+item).fadeIn();
+        });
+    } else {
+         $("#content").fadeIn(3000);
+         $("#content_text"+item).fadeIn();
+    }   
+   
+    lastitem=item;
+};  
+
